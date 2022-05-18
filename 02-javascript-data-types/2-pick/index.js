0a9b8c9a5e91fa99c3e4b80cf5c8a27e9bf382b2
Evgenii Alekseev <alexeevev@gmail.com>
2022-05-18 20:26:06 +0300

    02 | 2-pick, 3-omit (Task completed)

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,20 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
+export const pick = (...props) => {
 
+    const [obj, ...fields] = props;
+
+    const searchKeys = (fields, obj) => {
+        const newArrResults = [];
+        fields.forEach(fieldKey => {
+            Object.entries(obj).map(([key, value]) => {
+                if (key.includes(fieldKey)) {
+                    newArrResults.push([key, value])
+                }
+            });
+        });
+        return Object.fromEntries(newArrResults);
+    }
+    return searchKeys(fields, obj);
 };
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,35 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
+export const omit = (...props) => {
+
+    const [obj, ...fields] = props;
+
+    const ignoreKeys = (fields, obj) => {
+        if (fields.length !== 0) {            
+            const newArrResults = [];
+            const filteredFields = fields => {
+                const allKeys = Object.keys(obj);
+                fields.forEach((field) => {                    
+                    if(allKeys.includes(field)) {
+                        allKeys.splice(allKeys.indexOf(field), 1);
+                        return;
+                    }
+                });   
+                return allKeys;    
+            };        
+    
+            filteredFields(fields).forEach(readyKey => {     
+                Object.entries(obj).map(([key, value]) => {
+                    if (key.includes(readyKey)) {                   
+                        newArrResults.push([key, value])
+                    }
+                });    
+            });
+            return Object.fromEntries(newArrResults);
+        }
+        return obj; 
+    };
+    return ignoreKeys(fields, obj);
 
 };
