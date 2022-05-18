b6c3c4594045f7afecc64caa8262d243e326699e
Evgenii Alekseev <alexeevev@gmail.com>
2022-05-18 13:29:10 +0300

    02 | 1-sort-strings - Task completed

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,27 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(...props) { 
 
-}
+    const [data, param = 'asc', ...other] = props;   
+
+    const sortableData = {
+        data: data,
+        sortDirection: param,
+    };
+
+    const newSortedArray = ({data, sortDirection}) => {   
+
+        const caseFirst = sortDirection === 'asc' ? 'upper' : 'lower';
+
+        const result = data.slice().sort((a, b) => {
+            return a.localeCompare(b, ['ru-RU', 'en-US'], {caseFirst: caseFirst})
+        });
+
+        return sortDirection === 'asc' ? result : result.reverse();      
+        
+    };
+
+    return newSortedArray(sortableData);
+
+};
