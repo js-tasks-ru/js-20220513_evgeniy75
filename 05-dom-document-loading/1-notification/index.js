bf2bf05bc2fa3a2d176cfef44870baa07f514a1a
Evgenii Alekseev <alexeevev@gmail.com>
2022-05-31 15:16:44 +0300

    05 | 1 (Task completed)

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
 
+    constructor(
+        message = '', {
+            duration = 0,
+            type = '' } = {}
+    ) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.render();
+    }
+
+    template() {
+        return `
+        <div class="notification ${this.type}" style="--value:${this.duration}s">
+            <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">success</div>
+                <div class="notification-body">
+                ${this.message}
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    show(el) {
+        if (el) {
+            el.innerHTML = this.template();
+        }
+        this.closeMessage();
+    }
+
+    render() {
+        const el = document.createElement('div')
+        el.innerHTML = this.template()
+        this.element = el.firstElementChild
+        this.closeMessage();
+    }
+
+    closeMessage() {
+        if (this.duration) {
+            return setTimeout(() => { this.remove() }, this.duration);
+        }
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.element.remove();
+    }
+
 }
